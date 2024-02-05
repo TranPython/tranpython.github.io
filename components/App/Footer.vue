b7347f6b69dd4bf48ce05728590c965e1436067f
Tran Python <tranvanthong.hd@gmail.com>
2024-02-06 01:14:05 +0900

    Update Footer.vue

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -3,6 +3,6 @@
     class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8"
   >
     <br>
-    <p>© 2023 Fayaz Ahmed. All rights reserved.</p>
+    <p>© 2024 Tran Thong. All rights reserved.</p>
   </footer>
 </template>
